6120bf5a3d0be8e3cfbf0e6de877a6e0553f25b0
Dmitriy <akhmetov.dy@gmail.com>
2017-12-13 17:05:47 +0300

    Merge branch 'master' of https://github.com/alpiiscky/yii2-simple-helpers
    # Conflicts:
    #	README.md

---
+++ AdminHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace alpiiscky\simplehelpers;
+
+class AdminHelper
+{
+    public static function pre($mixed_data)
+    {
+        print '<pre style="white-space: pre-wrap; white-space: -moz-pre-wrap; white-space: -o-pre-wrap; background: #faf8f0; border: 1px solid silver; 
+                    padding: 2px; font-weight: normal; font-family: Monospace, Courier">';
+                print_r($mixed_data);
+        print '</pre>';
+    }
+    public static function code($mixed_data)
+    {
+        print '<code style="display:block;padding:0.5em 1em; border: 1px solid #bebab0;">';
+        print_r($mixed_data);
+        print '</code>';
+    }
+}
---
+++ UrlHelper.php
@@ -0,0 +1,22 @@
+<?php
+namespace alpiiscky\simplehelpers;
+
+class UrlHelper
+{
+    public static function createSlug($source)
+    {
+        $source = trim(mb_strtolower($source));
+        $translateArray = [
+            "ый" => "y", "а" => "a", "б" => "b",
+            "в" => "v", "г" => "g", "д" => "d", "е" => "e", "ж" => "j",
+            "з" => "z", "и" => "i", "й" => "y", "к" => "k", "л" => "l",
+            "м" => "m", "н" => "n", "о" => "o", "п" => "p", "р" => "r",
+            "с" => "s", "т" => "t", "у" => "u", "ф" => "f", "х" => "h",
+            "ц" => "c", "ч" => "ch", "ш" => "sh", "щ" => "sch", "ъ" => "",
+            "ы" => "y", "ь" => "", "э" => "e", "ю" => "yu", "я" => "ya",
+            " " => "-", "." => "", "/" => "-", "_" => "-"
+        ];
+        $source = preg_replace('#[^a-z0-9\-]#is', '', strtr($source, $translateArray));
+        return trim(preg_replace('#-{2,}#is', '-', $source));
+    }
+}
